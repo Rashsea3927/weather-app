c6f7fffb4493883f5ece87580e30ea588efc1afa
Rashsea <kaji.dev3927@gmail.com>
2025-09-11 22:46:16 +0900

    feat(ui): create header component

---
+++ components/layout/Header.tsx
@@ -0,0 +1,13 @@
+import SelectUnits from '@/components/SelectUnits';
+import Image from 'next/image';
+
+const Header = () => {
+  return (
+    <header className='flex items-center justify-between mb-12 md:mb-16'>
+      <Image className='w-auto h-7 md:h-10' priority src='/assets/logo.svg' width={197} height={40} alt='Weather Now' />
+      <SelectUnits />
+    </header>
+  );
+};
+
+export default Header;
